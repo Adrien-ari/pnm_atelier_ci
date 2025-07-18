574bed2a0da5e13a1da5fe3246930e10543755d8
kantoniaina <kantoniaina.nomenjanahary@student.passerellesnumriques.org>
2025-07-18 17:05:33 +0300

    new commit test converted

--- test.py
+++ test.py
@@ -14,3 +14,7 @@ def test_setbalance():
     obj = Wallet(0)
     obj.set_balance(40)
     assert obj.get_balance() == 40
+
+def test_converted_balance():
+    obj = Wallet(50)
+    assert obj.convert_balance(5000) == 250000
